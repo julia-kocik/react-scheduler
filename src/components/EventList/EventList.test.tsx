5a3d32d9b729ea64aa18e1e4fd14d50522cb4340
julia-kocik <julia.kocik@gmail.com>
2023-11-18 12:51:47 +0100

    Add EventList test

--- src/components/EventList/EventList.test.tsx
+++ src/components/EventList/EventList.test.tsx
@@ -1,15 +1,39 @@
-/**
- * @jest-environment jsdom
- */
 import { fireEvent, render, screen } from '@testing-library/react'
 import EventList from './EventList'
 import '@testing-library/jest-dom'
 import { Dispatch, SetStateAction } from 'react';
 
-it('App Router: Works with Client Components (React State)', () => {
+describe('EventList', () => {
   const setForceFetchAfterDelete: Dispatch<SetStateAction<boolean>> = jest.fn();
   const setForceFetchAfterUpdate: Dispatch<SetStateAction<boolean>> = jest.fn();
-
   const setToastInfo: Dispatch<SetStateAction<object>> = jest.fn();
-  render(<EventList setToastInfo={setToastInfo} setForceFetchAfterDelete={setForceFetchAfterDelete} setForceFetchAfterUpdate={setForceFetchAfterUpdate} eventsData={[]} />)
-})
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders EventList component', () => {
+    render(
+      <EventList 
+        setToastInfo={setToastInfo} 
+        setForceFetchAfterDelete={setForceFetchAfterDelete} 
+        setForceFetchAfterUpdate={setForceFetchAfterUpdate} 
+        eventsData={[]} 
+      />
+    )
+    expect(screen.getByText(/No events saved yet/)).toBeInTheDocument();
+  });
+
+  test('renders EventList component', () => {
+    render(
+      <EventList 
+        setToastInfo={setToastInfo} 
+        setForceFetchAfterDelete={setForceFetchAfterDelete} 
+        setForceFetchAfterUpdate={setForceFetchAfterUpdate} 
+        eventsData={[{id: '123', name: 'Zosia', surname: 'Kocik', email: 'john.doe@example.com', date: new Date()}]} 
+      />
+    )
+    expect(screen.queryByText(/No events saved yet/)).toBeNull();
+    expect(screen.getByText(/Zosia/)).toBeInTheDocument();
+  });
+});
